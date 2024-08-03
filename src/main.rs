0a1d683b37a2bac92a48cb8b9dc16ce5f21a6d07
Nishant Sthalekar <nishant06jun@gmail.com>
2024-08-03 16:51:48 -0400

    मम प्रथमं समर्पितम्।
    Initial push to what will be 'eventually' a game engine.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate sdl2;
+extern crate gl;
+
+fn main() {
+   println!("Sab Moha Maya hai!");
+   
+   let sdl = sdl2::init().unwrap();
+
+   let video_subsystem = sdl.video().unwrap();
+   
+   let gl_attr = video_subsystem.gl_attr();
+
+   gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+   gl_attr.set_context_version(4, 5);
+
+   let window = video_subsystem
+        .window("Mithya", 900, 700)
+        .opengl() // add opengl
+        .resizable()
+        .build()
+        .unwrap();
+
+   let gl_context = window.gl_create_context().unwrap();
+   let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+   
+   let mut event_pump = sdl.event_pump().unwrap();
+   'main: loop {
+      for event in event_pump.poll_iter() {
+            match event {
+               sdl2::event::Event::Quit {..} => break 'main,
+               _ => {},
+            }
+      }
+
+      // render window contents here
+      unsafe {
+         gl::Viewport(0, 0, 900, 700); // set viewport
+         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+         gl::Clear(gl::COLOR_BUFFER_BIT);
+     }
+
+     window.gl_swap_window();
+   }
+}
