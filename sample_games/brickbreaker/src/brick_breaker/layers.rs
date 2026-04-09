c52a4aa9d7fe7ce14c770b7342c2f99581edfd2d
Nishant Sthalekar <nishant06jun@gmail.com>
2026-04-08 21:50:00 -0400

    Moved brickbreaker to its own repo

--- sample_games/brickbreaker/src/brick_breaker/layers.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025 Nishant Sthalekar
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-pub const LAYER_WALL: u32   = 0b0001;
-pub const LAYER_BALL: u32   = 0b0010;
-pub const LAYER_PADDLE: u32 = 0b0100;
-pub const LAYER_BRICK: u32  = 0b1000;
